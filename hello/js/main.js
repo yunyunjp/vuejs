b2596426f8b3f5151295f5500843a6ddc1c03130
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-16 16:38:13 +0900

    initial commit

---
+++ hello/js/main.js
@@ -0,0 +1,65 @@
+Vue.filter('toUSD', function(jpy) {
+  return jpy / 100
+})
+Vue.filter('numberFormat', function(value) {
+  return value.toLocaleString()
+})
+
+Vue.filter('readMore', function(text, length, suffix){
+  return text.substring(0, length) + suffix
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    basePrice: 100,
+    url: 'https://www.google.co.jp',
+    message: 'Hello <span style="color:red;">Vue.js!</span>',
+    text: 'Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.',
+    price: 298000000,
+    jpyPrice: 298000,
+    number: 100,
+    ok: false,
+    count: 0,
+    user: {
+      lastName: 'Yamada',
+      firstName: 'taro',
+      prefecture: 'Tokyo'
+    },
+    colors: ['Red', 'Green', 'Blue']
+  },
+  methods: {
+    methodsNumber: function(){
+      console.log('methods!')
+      return Math.random()
+    },
+    clickHandler: function(event) {
+      this.message = this.message.split('').reverse().join('')
+    },
+    reversedMessageMethod: function() {
+      return this.message.split('').reverse().join('')
+    }
+  },
+  // filters: {
+  //   numberFormat: function(value) {
+  //     return value.toLocaleString()
+  //   }
+  // }
+  computed: {
+    computedNumber: function() {
+      console.log('computed!')
+      return Math.random()
+    },
+    reversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    },
+    taxIncludedPrice: {
+      get: function() {
+        return parseInt(this.basePrice * 1.08)
+      },
+      set: function(taxIncludedPrice) {
+        this.basePrice = Math.ceil(taxIncludedPrice / 1.08)
+      }
+    }
+  }
+})
